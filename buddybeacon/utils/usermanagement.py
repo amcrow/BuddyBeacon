cd01db2114ffe3dda2f8e95f3c65bf1ef2e280c2
Austin Crowley <devnodegree@gmail.com>
2018-06-17 23:32:43 -0500

    added user id initiation function and began coding User init function

--- buddybeacon/utils/usermanagement.py
+++ buddybeacon/utils/usermanagement.py
@@ -4,16 +4,23 @@ author: Austin M. Crowley
 author_email: devnodegree@gmail.com
 '''
 
+import hashlib
+
 class User():
 
-    def __init__(self):
-        pass
+    def __init__(self, username):
+        self.username=username
+        self.__set_user_id()
+
 
     def __repr__(self):
         pass
 
     def __str__(self):
         pass
+
+    def __set_user_id(self):
+        return hashlib.sha256(self.username)
 
 class UserSession():
 
