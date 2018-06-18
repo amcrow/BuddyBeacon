9984ad74afc462101597e30d3b3bf9c2bf3add83
Austin Crowley <devnodegree@gmail.com>
2018-06-17 22:57:03 -0500

    added various class-containing .py files

---
+++ buddybeacon/utils/usermanagement.py
@@ -0,0 +1,16 @@
+'''
+name: usermanagement.py
+author: Austin M. Crowley
+author_email: devnodegree@gmail.com
+'''
+
+class UserSession():
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils import setup
+
+setup(
+    name=='BuddyBeacon',
+    version=='0.0.0',
+    description=="There's safety in numbers!"
+    author=='Austin M. Crowley',
+    author email='devnodegree@gmail.com',
+    packages=[]
+    install_requires=[]
+)
