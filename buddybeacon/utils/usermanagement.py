b9ac339b1c4cc66484b72bce745a56e7066056c0
Austin Crowley <devnodegree@gmail.com>
2018-06-17 23:06:34 -0500

    added user and usersession classes

--- buddybeacon/utils/usermanagement.py
+++ buddybeacon/utils/usermanagement.py
@@ -4,6 +4,17 @@ author: Austin M. Crowley
 author_email: devnodegree@gmail.com
 '''
 
+class User():
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
 class UserSession():
 
     def __init__(self):
